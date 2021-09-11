22aad43fca9c4c81d1150a9b84d114e76d520e45
profoundsea25 <profoundsea25@gmail.com>
2021-09-11 15:12:27 +0900

    Sorting 21.09.11

--- Q26.py
+++ Q26.py
@@ -1,4 +1,5 @@
 # Q 26 카드 정렬하기, p.363 / 554
+# 시간 초과
 n = int(input())
 data = []
 for _ in range(n):
@@ -10,11 +11,17 @@ if n == 1 :
 elif n == 2:
     sum = data[0]+data[1]
 else:
-    sum = data[0]+data[1]
-    count = sum
-    for i in range(n-2):
-        sum += data[i+2]
+    sum = 0
+    count = 0
+    while len(data) != 1 :
+        sum += data[0]
+        data.pop(0)
+        sum += data[0]
+        data.pop(0)
         count += sum
+        data.append(sum)
+        data.sort()
+        sum = 0
     sum = count
 
 print(sum)
