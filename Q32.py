3abe482cd40f18b733fcec14dcefd97563f7127a
profoundsea25 <profoundsea25@gmail.com>
2021-10-08 00:54:31 +0900

    Dynamic Programming 21.10.08

---
+++ Q32.py
@@ -0,0 +1,26 @@
+n = int(input())
+
+array = []
+for i in range(n):
+    array.append(list(map(int, input().split())))
+
+for i in range(n-1) :
+    for j in range(i+1)
+
+
+array[0][0]+array[1][0]
+array[0][0]+array[1][1]
+
+array[1][0]+array[2][0]
+array[1][0]+array[2][1]
+array[1][1]+array[2][1]
+array[1][1]+array[2][2]
+
+2 0 + 3 0
+2 0 + 3 1
+2 1 + 3 1
+2 1 + 3 2
+2 1 + 3 1
+2 1 + 3 2
+2 2 + 3 2
+2 2 + 3 3
